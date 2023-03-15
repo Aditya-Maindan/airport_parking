52c0c60808c513bc711b70fd0f875f2953057927
Aditya Maindan <adityamaindan@Adityas-MacBook-Air.local>
2023-03-15 19:44:08 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Layout from './pages/Layout';
-import HomePage from './pages/HomePage';
+import Layout from './components/Layout';
 import Login from './pages/Login';
 import AirportAvailability from './pages/AirportAvailability';
- const App = () =>{
+import HomePage from './pages/Home';
+function App() {
   return (
    <BrowserRouter>
    <Routes>
